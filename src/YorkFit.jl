6f32e6c0e7d90c385e02f2650a1d6042653a6c58
Alex Jackson <alex@alexj.org>
2016-10-19 18:14:39 +0100

    Add least-squares function

--- src/YorkFit.jl
+++ src/YorkFit.jl
@@ -1,5 +1,13 @@
 module YorkFit
 
 # package code goes here
+"""Calculate `a` and `b` using the method of least squares. Returns the tuple
+`(a, b)` where `a` is the intercept and `b` is the gradient."""
+function lsq{T<:Real}(Xs::Vector{T}, Ys::Vector{T})::Tuple{T,T}
+    A = hcat(ones(length(Xs)), Xs)
+    m = (A' * A) \ A' * Ys
+
+    (m[1], m[2])
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using YorkFit
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# Test least-squares estimate
+let
+    Xs = collect(0:10.0)
+    Ys = 2.0 * Xs + 3.0
+    (a, b) = YorkFit.lsq(Xs, Ys)
+    @test isapprox(a, 3.0)
+    @test isapprox(b, 2.0)
+end
+
