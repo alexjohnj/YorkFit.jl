c47b998e9e477cdec2f19f0af313a6948b21e12e
Alex Jackson <alex@alexj.org>
2016-10-19 18:34:55 +0100

    Add helper function for York method

--- src/YorkFit.jl
+++ src/YorkFit.jl
@@ -1,6 +1,18 @@
 module YorkFit
 
-# package code goes here
+"""Calculate the weight of the error `σX`."""
+ω(σX::Real)::Real = σX^-2
+α{T<:Real}(ωX::T, ωY::T)::T = sqrt(ωX * ωY)
+W{T<:Real}(ωX::T, ωY::T, r::T, b::T, α::T)::T =
+    (ωX * ωY) / (ωX + b^2 * ωY - 2 * b * r * α)
+
+"""Compute Σ(Ws * Xs) / Σ(Xs)"""
+barvar{T<:Real}(Ws::Vector{T}, Xs::Vector{T})::T = sum(Ws .* Xs) / sum(Ws)
+
+function β{T<:Real}(W::T, U::T, V::T, ωX::T, ωY::T, b::T, r::T, α)::T
+    W * ((U / ωY) + (b * V / ωX) - (r / α) * (b * U + V))
+end
+
 """Calculate `a` and `b` using the method of least squares. Returns the tuple
 `(a, b)` where `a` is the intercept and `b` is the gradient."""
 function lsq{T<:Real}(Xs::Vector{T}, Ys::Vector{T})::Tuple{T,T}
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,41 @@ using Base.Test
 
 # Test least-squares estimate
 let
-    Xs = collect(0:10.0)
+    Xs = collect(1:10.0)
     Ys = 2.0 * Xs + 3.0
     (a, b) = YorkFit.lsq(Xs, Ys)
     @test isapprox(a, 3.0)
     @test isapprox(b, 2.0)
 end
 
+# Test components of York method with simple cases
+let
+    Xs = collect(1:10.0)
+    Ys = 2.0*Xs + 3.0
+    σXs = fill(0.05, 10)
+    σYs = fill(0.02, 10)
+    r = 0.0
+
+    ωXs = YorkFit.ω.(σXs)
+    ωYs = YorkFit.ω.(σYs)
+
+    @test isapprox(ωXs, fill(400.0, 10))
+    @test isapprox(ωYs, fill(2500.0, 10))
+
+    αs = YorkFit.α.(ωXs, ωYs)
+    @test isapprox(αs, fill(1000.0, 10))
+
+    Ws = YorkFit.W.(ωXs, ωYs, fill(r, 10), 2.0, αs)
+    @test isapprox(Ws, fill(1250/13, 10))
+
+    barX = YorkFit.barvar(Ws, Xs)
+    barY = YorkFit.barvar(Ws, Ys)
+
+    @test isapprox(barX, 5.5)
+    @test isapprox(barY, 14.0)
+
+    Us = Xs - barX
+    Vs = Ys - barY
+    βs = YorkFit.β.(Ws, Us, Vs, ωXs, ωYs, 2.0, r, αs)
+    @test isapprox(βs, collect(-4.5:1:4.5))
+end
