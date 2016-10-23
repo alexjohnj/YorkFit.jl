17f9291a822d5463bf41a28d3fc6911f579f2963
Alex Jackson <alex@alexj.org>
2016-10-23 21:35:59 +0100

    Add tests for the fit methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,3 +41,57 @@ let
     βs = YorkFit.β.(Ws, Us, Vs, ωXs, ωYs, 2.0, r, αs)
     @test isapprox(βs, collect(-4.5:1:4.5))
 end
+
+# Test fit methods
+let
+    Xs = collect(1.0:10.0)
+    Ys = 2.0*Xs + 3.0
+    σX = 0.25
+    σY = 0.5
+    r = 0.25
+
+    # Expected results
+    Ea = 3.0
+    Eb = 2.0
+    Eσb = 0.06741998624
+    Eσa = 0.41833001326
+
+    function testvals(a, b, σa, σb)
+        @test isapprox(a, Ea)
+        @test isapprox(b, Eb)
+        @test isapprox(σa, Eσa)
+        @test isapprox(σb, Eσb)
+    end
+
+    # Vector r, constant σX, Vector σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, σX, fill(σY, 10), fill(r, 10), niter=2)
+    testvals(a, b, σa, σb)
+
+    # Constant r, constant σX, Vector σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, σX, fill(σY, 10), r, niter=2)
+    testvals(a, b, σa, σb)
+
+    # Vector r, vector σX, constant σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, fill(σX, 10), σY, fill(r, 10), niter=2)
+    testvals(a, b, σa, σb)
+
+    # Constant r, vector σX, constant σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, fill(σX, 10), σY, r, niter=2)
+    testvals(a, b, σa, σb)
+
+    # Vector r, constant σX, σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, σX, σY, fill(r, 10), niter=2)
+    testvals(a, b, σa, σb)
+
+    # Constant σX, σY, r
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, σX, σY, r, niter=2)
+    testvals(a, b, σa, σb)
+
+    # Constant r, vector σX, Vector σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, fill(σX, 10), fill(σY, 10), r, niter=2)
+    testvals(a, b, σa, σb)
+
+    # Vector r, vector σX, Vector σY
+    (a, b, σa, σb) = YorkFit.fit(Xs, Ys, fill(σX, 10), fill(σY, 10), fill(r, 10), niter=2)
+    testvals(a, b, σa, σb)
+end
